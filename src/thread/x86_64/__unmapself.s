bd493513107f79b3b51de13de1dd7354810e23b5
Dominic Chen <d.c.ddcc@gmail.com>
2020-02-05 01:16:25 -0500

    manually call __ccfi_syscall in asm code

--- src/thread/x86_64/__unmapself.s
+++ src/thread/x86_64/__unmapself.s
@@ -1,10 +1,33 @@
 /* Copyright 2011-2012 Nicholas J. Kain, licensed under standard MIT license */
 .text
+.extern __ccfi_syscall
 .global __unmapself
 .type   __unmapself,@function
 __unmapself:
+	mov %rdi,%rbx
+	mov %rsi,%r12
+
+	movl $1,%edi
+	mov %rdi,%gs:0x0
+	movl $2,%esi
+	mov %rsi,%gs:0x8
+	movl $3,%edx
+	mov %rdx,%gs:0x10
+	call __ccfi_syscall
+
+	mov %rbx,%rdi
+	mov %r12,%rsi
 	movl $11,%eax   /* SYS_munmap */
 	syscall         /* munmap(arg2,arg3) */
-	xor %rdi,%rdi   /* exit() args: always return success */
+
+	movl $2,%edi
+	mov %rdi,%gs:0x0
+	movl $3,%esi
+	mov %rsi,%gs:0x8
+	movl $4,%edx
+	mov %rdx,%gs:0x10
+	call __ccfi_syscall
+
+	xor %edi,%edi   /* exit() args: always return success */
 	movl $60,%eax   /* SYS_exit */
 	syscall         /* exit(0) */

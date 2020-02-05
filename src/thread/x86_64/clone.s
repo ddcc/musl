b9ab3bedd1fb50c4ccda42664ac6a6821b1ce0c5
Dominic Chen <d.c.ddcc@gmail.com>
2020-02-05 01:16:25 -0500

    manually call __ccfi_syscall in asm code

--- src/thread/x86_64/clone.s
+++ src/thread/x86_64/clone.s
@@ -1,26 +1,56 @@
 .text
+.extern __ccfi_syscall
 .global __clone
 .hidden __clone
 .type   __clone,@function
 __clone:
+	mov %rdi,%rbx
+	mov %rsi,%r12
+	mov %rdx,%r13
+	push %rcx
+	mov %r8,%r14
+	mov %r9,%r15
+
+	movl $3,%edi
+	mov %rdi,%gs:0x0
+	movl $4,%esi
+	mov %rsi,%gs:0x8
+	movl $5,%edx
+	mov %rdx,%gs:0x10
+	call __ccfi_syscall
+
+	mov %rbx,%r9
+	mov %r12,%rsi
+	mov %r13,%rdi
+	pop %rcx
+	mov %r14,%rdx
+	mov %r15,%r8
+
 	xor %eax,%eax
 	mov $56,%al
-	mov %rdi,%r11
-	mov %rdx,%rdi
-	mov %r8,%rdx
-	mov %r9,%r8
 	mov 8(%rsp),%r10
-	mov %r11,%r9
 	and $-16,%rsi
 	sub $8,%rsi
 	mov %rcx,(%rsi)
 	syscall
+
 	test %eax,%eax
 	jnz 1f
 	xor %ebp,%ebp
 	pop %rdi
 	call *%r9
-	mov %eax,%edi
+
+	mov %eax,%ebx
+
+	movl $4,%edi
+	mov %rdi,%gs:0x0
+	movl $5,%esi
+	mov %rsi,%gs:0x8
+	movl $6,%edx
+	mov %rdx,%gs:0x10
+	call __ccfi_syscall
+
+	mov %ebx,%edi
 	xor %eax,%eax
 	mov $60,%al
 	syscall

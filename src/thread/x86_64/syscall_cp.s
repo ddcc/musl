09993c502e5d0fb4dde6edd3cd25abd772b68f20
Dominic Chen <d.c.ddcc@gmail.com>
2020-02-05 01:16:25 -0500

    manually call __ccfi_syscall in asm code

--- src/thread/x86_64/syscall_cp.s
+++ src/thread/x86_64/syscall_cp.s
@@ -1,4 +1,5 @@
 .text
+.extern __ccfi_syscall
 .global __cp_begin
 .hidden __cp_begin
 .global __cp_end
@@ -10,17 +11,31 @@
 .hidden __syscall_cp_asm
 .type   __syscall_cp_asm,@function
 __syscall_cp_asm:
+	mov %rdi,%rbx
+	mov %rsi,%r12
+	mov %rdx,%r13
+	mov %rcx,%r14
+	mov %r8,%r15
+	push %r9
+
+	movl $5,%edi
+	mov %rdi,%gs:0x0
+	movl $6,%esi
+	mov %rsi,%gs:0x8
+	movl $7,%edx
+	mov %rdx,%gs:0x10
+	call __ccfi_syscall
 
 __cp_begin:
-	mov (%rdi),%eax
+	mov (%rbx),%eax
 	test %eax,%eax
 	jnz __cp_cancel
-	mov %rdi,%r11
-	mov %rsi,%rax
-	mov %rdx,%rdi
-	mov %rcx,%rsi
-	mov %r8,%rdx
-	mov %r9,%r10
+	mov %rbx,%r11
+	mov %r12,%rax
+	mov %r13,%rdi
+	mov %r14,%rsi
+	mov %r15,%rdx
+	pop %r10
 	mov 8(%rsp),%r8
 	mov 16(%rsp),%r9
 	mov %r11,8(%rsp)

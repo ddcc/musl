fd7148ffa4125dec46a7d4503d0d658b35070cc2
Dominic Chen <d.c.ddcc@gmail.com>
2021-03-31 23:06:57 -0400

    add futex function and whitelist futex system calls as uninstrumented

--- src/internal/pthread_impl.h
+++ src/internal/pthread_impl.h
@@ -169,14 +169,14 @@ static inline void __wake(volatile void *addr, int cnt, int priv)
 {
 	if (priv) priv = FUTEX_PRIVATE;
 	if (cnt<0) cnt = INT_MAX;
-	__syscall(SYS_futex, addr, FUTEX_WAKE|priv, cnt) != -ENOSYS ||
-	__syscall(SYS_futex, addr, FUTEX_WAKE, cnt);
+	__hq_raw_syscall3(SYS_futex, (uintptr_t)addr, FUTEX_WAKE|priv, cnt) != -ENOSYS ||
+	__hq_raw_syscall3(SYS_futex, (uintptr_t)addr, FUTEX_WAKE, cnt);
 }
 static inline void __futexwait(volatile void *addr, int val, int priv)
 {
 	if (priv) priv = FUTEX_PRIVATE;
-	__syscall(SYS_futex, addr, FUTEX_WAIT|priv, val, 0) != -ENOSYS ||
-	__syscall(SYS_futex, addr, FUTEX_WAIT, val, 0);
+	__hq_raw_syscall4(SYS_futex, (uintptr_t)addr, FUTEX_WAIT|priv, val, 0) != -ENOSYS ||
+	__hq_raw_syscall4(SYS_futex, (uintptr_t)addr, FUTEX_WAIT, val, 0);
 }
 
 hidden void __acquire_ptc(void);
